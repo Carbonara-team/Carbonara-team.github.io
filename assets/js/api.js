2861aae702ffa3263ab1fa47e82a1afc3f0040bb
Krerkkiat Wattanaporn <s.icebear999@gmail.com>
2022-10-31 17:01:23 +0700

    1st Time

---
+++ assets/js/api.js
@@ -0,0 +1,38 @@
+fetch("https://gamertocoder.garena.co.th/api/minigames")
+ .then((response) => {
+ if (response.status !== 200){
+ return response.status;
+ }
+ return response.json();
+ })
+ .then((data) => {
+ if (typeof data == "number") {
+ alert(data);
+ }
+ else {
+    for (let i = 0; i < data.length; i++) {
+    const currentData = data[i]; 
+    const listItem = document.createElement("li");
+    listItem.classList.add("card");
+    const genre_array = currentData.genre;
+    let genre_string = genre_array[0];
+
+    if (genre_array.length > 1) { 
+        for (let j = 1; j < genre_array.length; j++) { 
+        genre_string = genre_string + genre_array[j]; 
+        }
+       }
+    
+    
+       const html =
+        '<div class="name" onclick="changeName(' + currentData.name +')">' + currentData.name + '</div>'
+        + '<img src="' + currentData.icon + '"/id="gameimage">'
+        + '<div class="cata">Catagories : ' + genre_string + '</div>'
+        + '<div class="detail">' + currentData.description + '</div>'
+
+        listItem.innerHTML = html;
+        document.getElementById("list").appendChild(listItem);
+        }
+        }
+       });
+       
